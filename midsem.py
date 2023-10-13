6074f71dfbb2d67491f442c248a7722d8fcbe881
Rohit Roy <92018099+scarroy-02@users.noreply.github.com>
2023-10-13 18:19:11 +0530

    Update midsem.py

--- midsem.py
+++ midsem.py
@@ -7,6 +7,7 @@ import time
 import multiprocessing
 from tqdm import tqdm
 
+np.random.seed(17)
 
 def H(N,a,x):
     num = math.sqrt(N)
@@ -42,7 +43,7 @@ def f(x):
 #print(f_6(x))
 #print(f(x))
 
-def a_k(k):
+def a_k_naive(k):
     def y(t,k):
         return (2 * t - 1)/(2 * k)
     sum = 0
@@ -55,27 +56,83 @@ def a_k(k):
                             sum = sum + f([y(t_1,k),y(t_2,k),y(t_3,k),y(t_4,k),y(t_5,k),y(t_6,k)])
     return sum/math.pow(k,6)
 
-def a_k_1(k):
+
+# Multi processing for a_k
+
+def a_k(k,n,i):
     def y(t, k):
         return (2 * t - 1) / (2 * k)
 
     sum_val = 0
-    constant_factor = 1 / math.pow(k, 6)
 
-    start_time = time.time()
+    # start_time = time.time()
 
-    for t in itertools.product(range(1, k+1), repeat=6):
+    #for i in tqdm(range(n)):
+    all_combos = itertools.product(range(1,k +1), repeat=6)
+    part_combos = itertools.islice(all_combos, i*((k**6)//n), (i+1)*((k**6)//n))
+
+    for t in tqdm(part_combos, desc = f"#{i}", total = (k**6)//n):
         y_values = [y(t_i, k) for t_i in t]
         sum_val += f(y_values)
+
+    # end_time = time.time()  # Record the end time
+    # execution_time = end_time - start_time
+
+    # print(f"Execution time: {execution_time:.4f} seconds")
+
+    return sum_val
+
+def worker_fn_a_k(k, n, i, queue):
+    result = a_k(k ,n, i)
+    queue.put(result)
+
+def a_k_multi(k, n):
+    result_queue = multiprocessing.Queue()
+
+    processes = []
+
+    #pbar = tqdm(total = k**2, desc="Samples done : ")
+
+    start_time = time.time()
+    # Start multiple processes and calculate results
+    for i in range(n):
+        process = multiprocessing.Process(target=worker_fn_a_k, args=(k,n,i, result_queue))
+        processes.append(process)
+        process.start()
+
+    # Wait for all processes to complete
+    for process in processes:
+        process.join()
 
     end_time = time.time()  # Record the end time
     execution_time = end_time - start_time
 
     print(f"Execution time: {execution_time:.4f} seconds")
 
-    return sum_val * constant_factor
+    # Sum the elements in the queue
+    total = 0
 
-def b_k(k):
+    while not result_queue.empty():
+        total += result_queue.get()
+        # print(item)
+        # total += item
+
+    constant_factor = 1 / math.pow(k, 6)
+    total = total * constant_factor
+
+    return total
+
+#print(a_k_multi(40,4))
+#print(a_k_naive(12))
+
+
+###############################################
+# From this section, it is for calculating b_k
+###############################################
+
+# We divide the sample into k^4 uniform 6-tuples, which we will iterate over k^2 times, due to size constraints.
+
+def b_k_single(k):
     start_time = time.time()
     sum = 0
     for i in range(k**3):
@@ -97,40 +154,33 @@ def b_k(k):
     print(f"Execution time: {execution_time:.4f} seconds")
     return sum/math.pow(k,6)
 
-# print(b_k(40))
-#print(a_k(40))
+# Multiprocessing starts from here.
 
-# print(a_k_1(40))
-
-
-
-def b_k_l(k,  n, pbar):
+def b_k(k, n, i):
     sum = 0
-    for i in range(k**3//n):
-        sample = np.random.uniform(size = (int(math.pow(k,3)),6))
+    for i in tqdm(range(k**2//n),desc=f"#{i}"):
+        sample = np.random.uniform(size = (int(math.pow(k,4)),6))
         sum += np.sum(np.array([f(sam) for sam in sample]))
 
         # if i%100 == 0:
         #     print(f"{i} samples done")
-
-        pbar.update(8)
     return sum/math.pow(k,6)
 
-def worker_fn(k, n, queue, pbar):
-    result = b_k_l(k ,n, pbar)
+def worker_fn_b_k(k, n, i, queue):
+    result = b_k(k ,n, i)
     queue.put(result)
 
-def multi(k, n):
+def b_k_multi(k, n):
     result_queue = multiprocessing.Queue()
 
     processes = []
 
-    pbar = tqdm(total = k**3, desc="Samples done : ")
+    #pbar = tqdm(total = k**2, desc="Samples done : ")
 
     start_time = time.time()
     # Start multiple processes and calculate results
     for i in range(n):
-        process = multiprocessing.Process(target=worker_fn, args=(k,n, result_queue, pbar))
+        process = multiprocessing.Process(target=worker_fn_b_k, args=(k,n,i, result_queue))
         processes.append(process)
         process.start()
 
@@ -148,7 +198,24 @@ def multi(k, n):
     while not result_queue.empty():
         total += result_queue.get()
 
-    print(f"Done! Sum = {total}")
+    return total
 
-# print(b_k(10))
-multi(40, 8)
+##################################################
+# Writing to file
+##################################################
+
+k_val = [10,12,14,16,18,20,25,30,36,40]
+
+with open(f"full_output.txt","w") as file:
+    for k in k_val:
+        if k == 25:
+            A_k = a_k_multi(k,5)
+            B_k = b_k_multi(k,5)
+        else:
+            A_k = a_k_multi(k,4)
+            B_k = b_k_multi(k,4)
+        file.write(f"k \t\t= {k}\n2^k \t= {2**k}\na_{k} \t= {A_k}\nb_k \t= {B_k}\n\n")
+
+file.close()
+
+print("Output Written to file.")
